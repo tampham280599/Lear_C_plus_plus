bb2454ac0ed43b0ed9e0e60b3ac3bb09cee3dc0a
tam.pham280599@gmail.com <tam.pham280599@gmail.com>
2022-11-15 22:07:33 +0700

    update

--- CODE/Data_Structure _Algorithms/Multi_Half/main.cpp
+++ CODE/Data_Structure _Algorithms/Multi_Half/main.cpp
@@ -6,11 +6,64 @@
 */
 
 /* a[0] = 1, i = 0, k1 = k2 = 0, a1, a2
-a1 = 2a[k1] + 1; a2 = 3a[k2] + 1   */
+a1 = 2a[k1] + 1; a2 = 3a[k2] + 1   
 a1 <= a2-- > a[i] = a1; k1 ++
 a1 >= a2-- > a[i] = a2; k2 ++
 
 a1 = 2 * 1 + 1 = 3; a2 = 3 * 1 + 1 = 4--->a1 < a2--> a[1] = 3, k1 = 1; k2 = 0;
 a1 = 2 * 3 + 1 = 7; a2 = 3 * 1 + 1 = 4 --->a1 > a2-- > a[2] = 4, k1 = k2 = 1;
 a1 = 2 * 3 + 1 = 7; a2 = 3 * 3 + 1 = 10->a[3] = 7; k1 = 2; k1 = 1;
-a1 = 2 * 4 + 1 = 9; a2 = 3 * 3 + 1 = 10;->a[4] = 9; k1 = 3; k2 = 1; 
+a1 = 2 * 4 + 1 = 9; a2 = 3 * 3 + 1 = 10;->a[4] = 9; k1 = 3; k2 = 1; */
+
+#include <iostream>
+#include <cstdint>
+
+int main()
+{
+	std::cout << "Building programing perform multi half" << "\n";
+	int16_t n;
+	int16_t i;
+	int k1 = 0;
+	int k2 = 0;
+	int a1;
+	int a2;
+	int a[100];
+
+	std::cout << "Enter n: ";
+	std::cin >> n;
+
+	std::cout << "print multi half: 1 ";
+	a[0] = 1;
+	for (i = 1; i < n; ++i)
+	{
+		a1 = 2 * a[k1] + 1;
+		a2 = 3 * a[k2] + 1;
+		if (a1 < a2)
+		{
+			a[i] = a1;
+			++k1;
+		}
+		else
+		{
+			a[i] = a2;
+			++k2;
+		}
+		std::cout << a[i] << ' ';
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
